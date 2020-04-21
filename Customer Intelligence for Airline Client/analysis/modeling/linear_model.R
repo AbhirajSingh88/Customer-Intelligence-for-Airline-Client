faabdbf47c369e32333078271e5b6d05d0932fac
AbhirajSingh88 <58116806+AbhirajSingh88@users.noreply.github.com>
2020-04-21 09:55:18 -0400

    Update linear_model.R

--- Customer Intelligence for Airline Client/analysis/modeling/linear_model.R
+++ Customer Intelligence for Airline Client/analysis/modeling/linear_model.R
@@ -1,5 +1,4 @@
-echo=F
-source("https://raw.githubusercontent.com/UKGANG/IST-687/master/mungling/dummy_builder.R")
+
 
 partnerFilter <- function(data, type) {
   return (filter(data, Flight.Ticket.Partner.Code %in% type));
